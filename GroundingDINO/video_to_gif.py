edb285698ed76b5fc891f36a10f789fda0a6ca8f
Lokesh Borawar <borawarlokesh26@gmail.com>
2024-09-23 16:26:36 +0530

    Add low res. video and gif for README.md.

--- GroundingDINO/video_to_gif.py
+++ GroundingDINO/video_to_gif.py
@@ -1,9 +1,27 @@
 from moviepy.editor import VideoFileClip
 
-video_path=r"GroundingDINO\asset\outputs\HD CCTV Camera video 3MP 4MP iProx CCTV HDCCTVCameras.net retail store.mp4"
+# Load the video
+video_path = r"GroundingDINO\asset\outputs\HD CCTV Camera video 3MP 4MP iProx CCTV HDCCTVCameras.net retail store.mp4"
+video = VideoFileClip(video_path)
+
+# Set the new width and calculate the corresponding height to maintain the aspect ratio
+new_width = 640  # Set the desired width
+aspect_ratio = video.size[1] / video.size[0]  # height / width
+new_height = int(new_width * aspect_ratio)
+
+# Resize the video while keeping the aspect ratio
+resized_video = video.resize(newsize=(new_width, new_height))
+
+# Save the resized video
+output_path = video_path
+resized_video.write_videofile(output_path)
+
+
+
+video_path=output_path
 # Load the video file
 clip = VideoFileClip(video_path)
-# Extract the segment from 3 to 9 seconds
+# Extract the segment from 35 to 45 seconds
 clip = clip.subclip(35, 45)
 # Convert to gif
 clip.write_gif(video_path.replace(".mp4",".gif"))
